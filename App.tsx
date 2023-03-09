9f0b586a9bb3c88712f0d52c3b9a72d8230621b0
blazingh <hadibaalbaki2@gmail.com>
2023-03-09 16:45:04 +0300

    gist

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { SafeAreaView } from "react-native";
+import { WebView } from "react-native-webview";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function MyWeb() {
+	return (
+		<SafeAreaView style={{ flex: 1 }}>
+			<WebView source={{ uri: "https://betaapi.dtsanalpos.com/payment" }} />
+		</SafeAreaView>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
