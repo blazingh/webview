17c8ec65d7403ee983aec3f6b592f8eeae491535
blazingh <hadibaalbaki2@gmail.com>
2023-03-14 12:32:40 +0300

    updated storage sync

--- App.tsx
+++ App.tsx
@@ -1,11 +1,60 @@
-import React, { Component } from "react";
-import { SafeAreaView } from "react-native";
-import { WebView } from "react-native-webview";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { WebView, WebViewMessageEvent } from "react-native-webview";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function MyWeb() {
+const MyWebView = () => {
+	const webViewRef = React.useRef<WebView>(null);
+	const [cookieString, setCookieString] = React.useState<any>(null);
+
+	const handleWebViewLoad = (event: WebViewMessageEvent) => {
+		AsyncStorage.setItem("cookies", event.nativeEvent.data);
+	};
+
+	const setWebViewCookie = async () => {
+		const injectedJavaScript = `document.cookie = '${cookieString}';  true;`;
+		webViewRef.current?.injectJavaScript(injectedJavaScript);
+	};
+
+	const setWebViewCookieString = `(${setWebViewCookie.toString()})();`;
+
+	const getData = async () => {
+		const value = await AsyncStorage.getItem("cookies");
+		if (value) setCookieString(value);
+		else setCookieString("empty");
+	};
+
+	React.useEffect(() => {
+		getData();
+	}, []);
+
+	const handleNavigationStateChange = async () => {
+		const webViewScript = `(function() {window.ReactNativeWebView.postMessage(document.cookie);})();`;
+		webViewRef.current?.injectJavaScript(webViewScript);
+	};
+
 	return (
-		<SafeAreaView style={{ flex: 1 }}>
-			<WebView source={{ uri: "https://betaapi.dtsanalpos.com/payment" }} />
-		</SafeAreaView>
+		<View style={styles.container}>
+			{cookieString && (
+				<WebView
+					mixedContentMode="always"
+					ref={webViewRef}
+					source={{ uri: "https://dtsanalpos.com/payment" }}
+					onMessage={handleWebViewLoad}
+					onNavigationStateChange={handleNavigationStateChange}
+					onShouldStartLoadWithRequest={() => true}
+					injectedJavaScriptBeforeContentLoaded={setWebViewCookieString}
+				/>
+			)}
+		</View>
 	);
-}
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingTop: 30,
+	},
+});
+
+export default MyWebView;
