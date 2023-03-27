407f9138976efcdfc7a428df59a77484f4043e38
blazingh <hadibaalbaki2@gmail.com>
2023-03-28 00:23:55 +0300

    added updated checker

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView, Text, Alert, Platform } from "react-native";
 import { WebView, WebViewMessageEvent } from "react-native-webview";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StatusBar } from "react-native";
+import * as Application from "expo-application";
+import { useEffect } from "react";
 
 const MyWebView = () => {
 	const webViewRef = React.useRef<WebView>(null);
 	const [cookieString, setCookieString] = React.useState<any>(null);
+	const [latestVersion, setLatestVersion] = React.useState<any>(null);
 
 	const handleWebViewLoad = (event: WebViewMessageEvent) => {
 		AsyncStorage.setItem("cookies", event.nativeEvent.data);
@@ -27,7 +30,29 @@ const MyWebView = () => {
 
 	React.useEffect(() => {
 		getData();
+
+		if (Platform.OS === "ios") {
+			fetch("https://betaapi.dtsanalpos.com/latest-ios-version")
+				.then((response) => response.json())
+				.then((json) => {
+					setLatestVersion(json.result.version);
+				})
+				.catch((error) => console.error(error));
+		}
 	}, []);
+
+	useEffect(() => {
+		if (!latestVersion) return;
+		if (latestVersion === Application.nativeApplicationVersion) return;
+		Alert.alert("New Version", "A new Version of the app is avaible", [
+			{
+				text: "Update Later",
+				onPress: () => console.log("Cancel Pressed"),
+				style: "cancel",
+			},
+			{ text: "Update", onPress: () => console.log("OK Pressed") },
+		]);
+	}, [latestVersion]);
 
 	const handleNavigationStateChange = async () => {
 		const webViewScript = `(function() {window.ReactNativeWebView.postMessage(document.cookie);})();`;
@@ -37,6 +62,8 @@ const MyWebView = () => {
 	return (
 		<>
 			<StatusBar backgroundColor="#7256E9" />
+			<Text>{Application.nativeApplicationVersion}</Text>
+			<Text>{latestVersion}</Text>
 			<SafeAreaView style={styles.container}>
 				{cookieString && (
 					<WebView
