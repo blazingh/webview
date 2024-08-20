d9af9c99bc5eeda887d296612ef135962ae59717
hadi baalbaki <hadibaalbaki2@gmail.com>
2024-08-20 08:40:31 +0000

    removed nfc and user tracking

--- App.tsx
+++ App.tsx
@@ -2,42 +2,22 @@ import React from "react";
 import {
 	StyleSheet,
 	SafeAreaView,
-	Alert,
-	Platform,
-	Linking,
 	View,
 	ActivityIndicator,
-	TouchableOpacity,
 	ImageBackground,
-	Text,
 } from "react-native";
-import { WebView, WebViewMessageEvent } from "react-native-webview";
+import { WebView } from "react-native-webview";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StatusBar } from "react-native";
-import * as Application from "expo-application";
-import { useEffect } from "react";
-import { Svg, Path } from "react-native-svg";
-// import { requestTrackingPermissionsAsync } from "expo-tracking-transparency";
-
 const spalshscrenn = require("./assets/splash.jpg");
 
 const MyWebView = () => {
 	const webViewRef = React.useRef<WebView>(null);
 	const [cookieString, setCookieString] = React.useState<any>(null);
-	const [latestVersion, setLatestVersion] = React.useState<any>(null);
 
 	const [loaded, setLoaded] = React.useState<boolean>(false);
 
 	const [visibleLoading, setVisibleLoading] = React.useState<boolean>(false);
-
-
-	const handleOnMessage = (event: WebViewMessageEvent) => {
-		if (event.nativeEvent.data === "INIT_NFC") {
-			// NFC is ready to be used
-			console.log("NFC is ready to be used");
-		}
-		AsyncStorage.setItem("cookies", event.nativeEvent.data);
-	};
 
 	const setWebViewCookie = async () => {
 		const injectedJavaScript = `
@@ -59,48 +39,11 @@ const MyWebView = () => {
 
 	React.useEffect(() => {
 		getData();
-
-		if (Platform.OS === "ios") {
-			fetch("https://betaapi.dtsanalpos.com/latest-ios-version")
-				.then((response) => response.json())
-				.then((json) => {
-					setLatestVersion(json.result.version);
-				})
-				.catch((error) => console.error(error));
-		}
-		// (async () => {
-		// 	const { status } = await requestTrackingPermissionsAsync();
-		// 	if (status === "granted") {
-		// 		console.log("permision granted");
-		// 	}
-		// })();
 	}, []);
-
-	useEffect(() => {
-		if (!latestVersion) return;
-		if (latestVersion === Application.nativeApplicationVersion) return;
-		Alert.alert("Yeni sürüm", "Uygulama için yeni sürüm mevcut", [
-			{
-				text: "Sonra güncelle",
-				onPress: () => console.log("Canceled update"),
-				style: "cancel",
-			},
-			{
-				text: "Şimdi güncelle",
-				onPress: () =>
-					Linking.openURL(
-						"https://apps.apple.com/tr/app/dtsanalpos/id6446242806?l=tr"
-					),
-			},
-		]);
-	}, [latestVersion]);
 
 	const handleNavigationStateChange = async (navState: any) => {
 		const webViewScript = `(function() {window.ReactNativeWebView.postMessage(document.cookie);})();`;
 		webViewRef.current?.injectJavaScript(webViewScript);
-
-		// if (navState.url.includes("dtsanalpos.com")) setBackButtonVsisble(true);
-		// else setBackButtonVsisble(false);
 	};
 
 	const handleOnLoad = () => {
@@ -166,7 +109,6 @@ const MyWebView = () => {
 							sharedCookiesEnabled={true}
 							mixedContentMode="always"
 							source={{ uri: "https://distedavim.com" }}
-							onMessage={handleOnMessage}
 							onNavigationStateChange={handleNavigationStateChange}
 							onShouldStartLoadWithRequest={() => true}
 							injectedJavaScriptBeforeContentLoaded={setWebViewCookieString}
